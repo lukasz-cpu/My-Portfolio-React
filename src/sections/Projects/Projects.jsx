5ac933a4791aa751b73d6e44fcce7cd197ad1b44
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-17 23:11:28 +0100

    Update Projects.jsx

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import styles from './ProjectStyles.module.css';
+import viberr from '../../assets/viberr.png'
 
 const Projects = () => {
     return (
-        <div>
-            Project
-        </div>
+        <section id="projects" className={styles.container}>
+            <h1 className='sectionTitle'>Projects</h1>
+            <div className={styles.projectsContainer}></div>
+            <a href='https://github.com/lukasz-cpu' target="_blank">
+            <img className="hover" src={viberr} alt="Viber Logo" />
+            <h3>Viberr</h3>
+            <p>Streaming App</p>
+            </a>
+        </section>
     );
 };
 
