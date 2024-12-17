b5283ca3b598d98676e265a939e4f3f1fbf3bc5e
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-17 22:20:12 +0100

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ReactDom from "react-dom/client";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
